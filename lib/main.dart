d3190ce5252bd239f1e3780a3d174899e91b936d
Chathra Navoda <chathra96@gmail.com>
2022-11-10 09:55:10 +0530

    Getex initialize

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:helpinghands/login_page.dart';
 import 'package:helpinghands/signup.dart';
 import 'package:helpinghands/welcome.dart';
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
           // This is the theme of your application.
@@ -27,6 +28,6 @@ class MyApp extends StatelessWidget {
           // is not restarted.
           primarySwatch: Colors.blue,
         ),
-        home: WelcomePage());
+        home: LoginPage());
   }
 }
